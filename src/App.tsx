2e12f7470cd397e0ab348386f2ee830a87b14c81
Chiefavefan <110802723+ZhehuanUnique@users.noreply.github.com>
2026-01-06 20:29:22 +0800

    Remove unused Login import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import Home from './pages/Home'
-import Login from './pages/Login'
 import TaskList from './pages/TaskList'
 import { AuthService } from './services/auth'
 import ScriptInput from './pages/ScriptInput'
